8142004bea9cf7316cc60e79691293246f88df73
HarvestLute <A01732792@tec.mx>
2022-05-11 00:27:38 +0000

    update 2

--- Analisis.py
+++ Analisis.py
@@ -2,5 +2,14 @@
 import pandas as pd
 # %%
 df = pd.read_csv("insurance.csv")
-df[10:11]
+df['age'].quantile(0.25)
+
+# %%
+df[['age']].quantile([0.25,0.50,0.75])
+# %%
+IQR = 51-27
+Left=27 - 1.5*IQR
+Right=51+1.5*IQR
+(Left, Right)
+
 # %%
